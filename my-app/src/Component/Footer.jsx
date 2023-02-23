b0b9782e0fdae5839b51d44f2743a69a8c6eb248
Magda <ma.wernik@gmail.com>
2023-02-23 16:47:22 +0100

    merge

--- my-app/src/Component/Footer.jsx
+++ my-app/src/Component/Footer.jsx
@@ -5,18 +5,10 @@ export const Footer = () => {
     
 <>
 
-<<<<<<< HEAD
 <footer class="p-4 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
     <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"> © 2023 WhyNot™ All Rights Reserved.
     </span>
    
-=======
-<footer class="p-4 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-white-800">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">©Copyright2023<a href="https://flowbite.com/" class="hover:underline"></a> Why Not All Rights Reserved.
-    </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm text-black-500 dark:text-gray-400 sm:mt-0">
-    </ul>
->>>>>>> 72564bd788eab099941b59a37c61b4d8225d02ce
 </footer>
 </>
   )
