109b7ed8bcabcd3eaa7fef089d30a122d9566099
AlejoR27 <alejandrobr27@hotmail.com>
2023-02-23 16:47:03 +0100

    probando carrusel

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
 import Home from './views/Home';
-import Card from './component/Card';
-import About from './component/About';
 
-import Login from './views/Login';
 
 
 function App() {
   return (
     <div className="App">
       <Home />
-    
-      <About />
-      
-      <Login />
     </div>
   );
 }
--- my-app/src/views/Home.jsx
+++ my-app/src/views/Home.jsx
@@ -2,19 +2,24 @@ import React from "react";
 import Navbar from "../component/Navbar";
 import Card from "../component/Card";
 import Footer from "../component/Footer";
+import Carrousel from "../component/Carrousel";
 
 function Home() {
   return (
     <>
-      <div
-        class="box-border 
-.m-0	margin: 0;		copy
-.m-0.5	margin: 0.125rem;"
-      >
-        <Navbar />
+    <Navbar />
+    <Carrousel />
+      <div className = 'main'>
         <Card />
-        <Footer />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
+      <Footer />
     </>
   );
 }
